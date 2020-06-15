32277b8f3b42e6ab7403b1c78de9ca9b20a2c414
Kfor <kfor1day@zju.edu.cn>
2020-06-15 11:09:10 +0800

    update register

--- questionnaireFrontEnd/vue.config.js
+++ questionnaireFrontEnd/vue.config.js
@@ -6,6 +6,10 @@ module.exports = {
                 target: 'http://localhost:8080',
                 changeOrigin: true,
             },
+            '/register':{
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+            }
 
         },
     },
